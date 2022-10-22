cacc27a3ab1177bbc90ce6954d92220635609644
maicol604 <maicol.fario@gmail.com>
2022-10-22 14:14:37 -0400

    Updates

---
+++ precache-manifest.627367abc43d6c8bf8181c1ca07faa52.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "2dfdc54e150fa46bda7b",
+    "url": "/airbnb-cpy/static/css/main.15a9780b.chunk.css"
+  },
+  {
+    "revision": "2dfdc54e150fa46bda7b",
+    "url": "/airbnb-cpy/static/js/main.2dfdc54e.chunk.js"
+  },
+  {
+    "revision": "ba6d0413e41a6571c81a",
+    "url": "/airbnb-cpy/static/js/1.ba6d0413.chunk.js"
+  },
+  {
+    "revision": "4b688dea190124ba57e5",
+    "url": "/airbnb-cpy/static/css/2.04d0af71.chunk.css"
+  },
+  {
+    "revision": "4b688dea190124ba57e5",
+    "url": "/airbnb-cpy/static/js/2.4b688dea.chunk.js"
+  },
+  {
+    "revision": "736a61b84cd20ec3a71b",
+    "url": "/airbnb-cpy/static/js/runtime~main.736a61b8.js"
+  },
+  {
+    "revision": "d301921234528fb503119e969866a882",
+    "url": "/airbnb-cpy/static/media/carousel (2).d3019212.jpeg"
+  },
+  {
+    "revision": "05efe386e527fde89a5f9affc39b3240",
+    "url": "/airbnb-cpy/static/media/carousel (1).05efe386.jpeg"
+  },
+  {
+    "revision": "cd436d00a7a98ff98620adb5c150e79c",
+    "url": "/airbnb-cpy/static/media/carousel (3).cd436d00.jpeg"
+  },
+  {
+    "revision": "cf3a18137446ca8a565ff0e7274142db",
+    "url": "/airbnb-cpy/index.html"
+  }
+];
